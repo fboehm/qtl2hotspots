e86bf159bef4e87e3ffd258c8d5848344f9fa9be
Frederick Boehm <frederick.boehm@gmail.com>
2019-08-08 15:03:57 -0500

    added doc to binning functions

--- R/bin_peak_position.R
+++ R/bin_peak_position.R
@@ -15,3 +15,27 @@ bin_peak_position <- function(peak_position, breaks){
   if (!is.numeric(breaks)) stop("breaks must be numeric")
   return(sum(peak_position > breaks) + 1)
 }
+
+#' Binarize bin number
+#'
+#' @param bin_number a positive integer
+#' @param total_number_of_bins a positive integer, at least as big as bin_number
+#' @return a binary vector with exactly one nonzero entry
+#' @export
+#' @examples
+#' binarize_bin_number(3, 10)
+#' binarize_bin_number(10L, 10L)
+#'
+binarize_bin_number <- function(bin_number, total_number_of_bins){
+  if (bin_number > total_number_of_bins) stop("bin_number must be less than or equal to bin_number")
+  if (bin_number < 1) stop("bin_number must be at least 1")
+  if (total_number_of_bins < 1) stop("total_number_of_bins must be at least 1")
+  if (!is.numeric(bin_number)) stop("bin_number must be an integer")
+  if (!is.numeric(total_number_of_bins)) stop("total_number_of_bins must be an integer")
+  if (length(bin_number) != 1) stop("bin_number must have length 1")
+  if (length(total_number_of_bins) != 1) stop("total_number_of_bins must have length 1")
+  # end checks
+  out <- rep(0, total_number_of_bins)
+  out[bin_number] <- 1
+  return(out)
+}
---
+++ tests/testthat/test-binarize_bin_number.R
@@ -0,0 +1,10 @@
+library(hotspots)
+library(testthat)
+
+
+test_that("binary vector is correct in simple cases", {
+  expect_equal(sum(binarize_bin_number(1, 2)), 1)
+  expect_length(binarize_bin_number(1, 2), 2)
+  expect_equal(class(binarize_bin_number(1, 2)), "numeric")
+})
+
